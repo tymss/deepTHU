1ddb6e739a1fd7f5caea80ae914c4a43f509449f
tymss <1042318957@qq.com>
2019-05-02 22:22:41 +0800

    finish api/v1 except upload & download

--- backend/serializer.py
+++ backend/serializer.py
@@ -5,18 +5,24 @@ class ErrorData(object):
     def __init__(self, err_info):
         self.error_info = err_info
 
-
 class ErrSerializer(serializers.Serializer):
     error_info = serializers.CharField(max_length=50)
-
 
 class TaskIdData(object):
     def __init__(self, task_id):
         self.task_id = task_id
 
-
 class TaskIdSerializer(serializers.Serializer):
     task_id = serializers.CharField(max_length=32)
+
+class TaskStateData(object):
+    def __init__(self, taskid, state):
+        self.task_id = taskid
+        self.task_state = state
+
+class TaskStateSerializer(serializers.Serializer):
+    task_id = serializers.CharField(max_length=32)
+    task_state = serializers.CharField(max_length=20)
 
 
 def get_err_response(err_info):
@@ -28,4 +34,10 @@ def get_err_response(err_info):
 def get_task_id_response(taskid):
     task = TaskIdData(task_id=taskid)
     serializer = TaskIdSerializer(task)
+    return serializer.data
+
+
+def get_task_state_response(id, state):
+    ts = TaskStateData(id, state)
+    serializer = TaskStateSerializer(ts)
     return serializer.data
--- backend/views.py
+++ backend/views.py
@@ -1,42 +1,76 @@
 from rest_framework.response import Response
-from .serializer import get_err_response
+from .serializer import get_err_response, get_task_id_response, get_task_state_response
 from rest_framework.decorators import api_view
 import uuid
 from .models import Task
 
+
 @api_view(['GET', 'POST', ])
 def src_upload_view(request):
-
     if request.method == 'POST':
         task_id = uuid.uuid4()
         obj, is_created = Task.objects.get_or_create(task_id=task_id)
         while not is_created:
             task_id = uuid.uuid4()
             obj, is_created = Task.objects.get_or_create(task_id=task_id)
-        print(task_id)
+
+        # TODO: upload file
+
     else:
-        return Response(get_err_response('Method Not Supported.'), status=400)
+        return Response(get_err_response('method %s is not supported.' % request.method), status=400)
+
 
 @api_view(['GET', 'POST', ])
 def dst_upload_view(request):
-
     if request.method == 'POST':
-        id = request.GET
+        id = request.GET.get('task_id')
         if id is None:
-            return Response(get_err_response('Parameter \'task_id\' Needed.'), status=400)
+            return Response(get_err_response('parameter \'task_id\' is needed.'), status=400)
+        try:
+            obj = Task.objects.get(task_id=id)
+        except Task.DoesNotExist:
+            return Response(get_err_response('task:%s is not found.' % id), status=404)
 
+        # TODO: upload file
+
+        obj.state = 'CREATED'
+        obj.save()
+        return Response(get_task_id_response(id), status=200)
 
     else:
-        return Response(get_err_response('Method Not Supported.'), status=400)
+        return Response(get_err_response('method %s not supported.' % request.method), status=400)
 
 
 @api_view(['GET', 'POST', ])
 def task_query_view(request):
+    if request.method == 'GET':
+        id = request.GET.get('task_id')
+        if id is None:
+            return Response(get_err_response('parameter \'task_id\' is needed.'), status=400)
+        try:
+            obj = Task.objects.get(task_id=id)
+        except Task.DoesNotExist:
+            return Response(get_err_response('task:%s is not found.' % id), status=404)
+        return Response(get_task_state_response(id, obj.state), status=200)
 
-    return 0
+    else:
+        return Response(get_err_response('method %s not supported.' % request.method), status=400)
 
 
 @api_view(['GET', 'POST', ])
 def task_result_view(request):
+    if request.method == 'GET':
+        id = request.GET.get('task_id')
+        if id is None:
+            return Response(get_err_response('parameter \'task_id\' is needed.'), status=400)
+        try:
+            obj = Task.objects.get(task_id=id)
+        except Task.DoesNotExist:
+            return Response(get_err_response('task:%s is not found.' % id), status=404)
+        if obj.state != 'FINISHED':
+            return Response(get_err_response('task:%s is not finished.' % id), status=409)
+        # TODO: download file
 
-    return 0
+
+    else:
+        return Response(get_err_response('method %s not supported.' % request.method), status=400)
